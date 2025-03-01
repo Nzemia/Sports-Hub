96e65b509107db2c22feb9741d04d9494ea0ca79
nzemia <mualukofrank@gmail.com>
2025-03-01 22:59:53 +0300

    set up backend

---
+++ api/models/userModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        skill: {
+            type: String
+        },
+        phoneNumber: {
+            type: String,
+            unique: true
+        },
+        otp: String,
+        noOfGames: {
+            type: Number,
+            default: 0
+        },
+        playPals: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        onlineStatus: {
+            type: String,
+            enum: ["online", "offline"],
+            default: "offline"
+        },
+        sports: [
+            {
+                type: String
+            }
+        ],
+        reviews: [
+            {
+                reviewer: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User"
+                },
+                rating: Number,
+                comment: String,
+                timestamp: { type: Date, default: Date.now }
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
